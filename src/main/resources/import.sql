b33cf07dfcfe6b522804f4999bf7c5ac16e67d6b
Monoki <monokizsolt7@gmail.com>
2019-10-09 11:27:13 +0200

    Add import.sql for test data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,14 @@
+INSERT INTO person(name, role) VALUES('Chris', 0);
+INSERT INTO person(name, role) VALUES('Peter', 0);
+INSERT INTO person(name, role) VALUES('Mr. John', 1);
+insert into university (name, address, popularity) values ('ELTE', 'Pázmány Péter sétány 1/C', 5);
+insert into university (name, address, popularity) values ('BME', 'Egry József u. 1', 4);
+insert into university (name, address, popularity) values ('Testnevelési egyetem', 'Alkotás u. 44, 1123', 2);
+
+insert into subject (name, code, description, university_id) values ('Alkalmazások fejlesztése', 'IK01', 'Spring boot és Angular fejlesztés',1);
+insert into subject (name, code, description, university_id) values ('Valószínűségszámítás', 'IK02', 'Kötelező',1);
+insert into subject (name, code, description, university_id) values ('Analízis 3.', 'IK03', 'Integrálás',1);
+insert into subject (name, code, description, university_id) values ('Analízis 6.', 'BME01', 'Modellezés',2);
+insert into subject (name, code, description, university_id) values ('Tenisz', 'TF01', 'Kezdő tenisz',3);
+
+insert into person_subject_list (person_list_id, subject_list_id) values (1, 1);
